c0d5bbaf397e21bda4881c46b4f331598bf23bad
mherhunanyan <mher.hunanyan2@gmail.com>
2024-07-07 20:36:52 +0400

    remove unnessory code

--- src/middlewares/IsAuthMiddleware.ts
+++ src/middlewares/IsAuthMiddleware.ts
@@ -1,16 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
 import LoggerFactory from 'logger/Logger.factory';
 import { redis } from 'database/Redis';
+import { USERID } from 'Constants';
 
 export const IsAuthMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const logger = LoggerFactory.getLogger('IsAuthMiddleware');
     try {
-        const { userAccessToken } = req.body;
-        if (!userAccessToken) {
+        const { accessToken } = req.body;
+        if (!accessToken) {
             res.status(401).json({ message: 'Access denied. No access token provided.' });
         }
-        const user = await redis.get(userAccessToken);
-        if (user) {
+
+        const isExistAccessToken = await redis.exists(accessToken);
+        if (isExistAccessToken) {
             return next();
         } else {
             return res.status(401).json({ message: 'Wrong access Token.' });
