4cf0c853ca8b4960e38f73afbd285f3e40719270
mherhunanyan <mher.hunanyan2@gmail.com>
2024-07-19 19:48:23 +0400

    improvments

--- src/utils/tokenUtils/HandleJwtError.ts
+++ src/utils/tokenUtils/HandleJwtError.ts
@@ -6,7 +6,7 @@ export const handleJwtError = (error: any, res: Response, logger: KibanaLogger) 
     if (error instanceof jwt.TokenExpiredError) {
         return res.status(403).json({ message: 'Refresh token expired.' });
     } else if (error instanceof jwt.JsonWebTokenError) {
-        return res.status(403).json({ message: 'Invalid refresh token.' });
+        return res.status(403).json({ message: 'Invalid token.' });
     } else {
         logger.error(error as string);
         return res.status(500).json({ message: 'Internal server error' });
